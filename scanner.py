fd4e0c78e31ca36b5866bf64ee0fe5f908b50112
root <root@localhost.localdomain>
2019-09-29 18:41:56 -0700

    prettified fast scan logging

--- scanner.py
+++ scanner.py
@@ -2,31 +2,33 @@
 
 import sys
 import subprocess
-import datetime
+import time
 
 if (len(sys.argv) < 2):
 	print("Call scanner with a target IP address")
-	print("Usage: scanner <ip> ?<output.file>")
+	print("Usage: scanner <ip> ?<output_filename>")
 	exit()
 
-print("Scanning ports of IP address {}".format(sys.argv[1]))
+scan_time = time.strftime('%Y-%m-%d_%H:%M:%S')
+init_time = time.strftime('%l:%M %p %Z on %b %d, %Y')
+print("-" * 64 + "\nScanning ports on {} at{}\n".format(sys.argv[1], init_time) + "-" * 64)
+
 command = ["nmap", "-oG", "-F", sys.argv[1]]
 if (len(sys.argv) < 3):
-	command.insert(2, "output.txt")
+	write_path = str(scan_time) + ".nmap"
+	command.insert(2, write_path)
 else:
 	command.insert(2, sys.argv[2])
 
 res = subprocess.check_output(command)
 
-# uncomment to display nmap output to terminal
-# ---------------------------------------------
-#for line in res.splitlines():
-#	print(line)
+for line in res.splitlines():
+	print(line)
 
 if (len(sys.argv) == 3):
 	print("Results of nmap scan stored in {}".format(sys.argv[2]))
 else:
-	print("Results of nmap scan stored in output.txt")
+	print("Results of nmap scan stored in {}.nmap".format(str(scan_time)))
 
 sys.exit()
 exit()
