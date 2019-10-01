eeb77520de4cb33eabe2aab957e49d9f2d6bbbd8
root <root@localhost.localdomain>
2019-09-30 20:57:55 -0700

    experiment w/ C disassembly

---
+++ HelloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+	int i;
+ 	for(i=0; i < 3; i++) { // iterate thrice
+ 		puts("Hello, world!\n");
+ 	}
+
+	return 0; // Tell OS the program exited without errors.
+}
